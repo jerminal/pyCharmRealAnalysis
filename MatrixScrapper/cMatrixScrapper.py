cc5e10f24ebfce77a287d63dea7b4254c64393e9
Xiaowei Tan <shutterfly@gmail.com>
2018-01-19 22:03:18 -0600

    started new matrix scrapper

--- MatrixScrapper/cMatrixScrapper.py
+++ MatrixScrapper/cMatrixScrapper.py
@@ -2,32 +2,26 @@ import XmlConfigReader
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class cMatrixScrapper:
     def __init__(self, strConfigFilePath, strConfigSect ):
         #initialize the object, and connect to har
+        print("Initialize Matrix scrapper")
         self._cfg = XmlConfigReader.Config(strConfigFilePath, strConfigSect)
         executable_path = self._cfg.getConfigValue("GeckoPath")
         strBrowser = self._cfg.getConfigValue("Browser")
+        print("Opening {0} browser".format(strBrowser))
         if strBrowser == "Chrome":
             self._driver = webdriver.Chrome()
         self.SignIntoMatrix()
-        (elemNextLnk, nFailureCnt) = self.find_wait_get_element("link_text", "Enter Matrix MLS")
-        window_before = self._driver.window_handles[0]
-        xpath = "/html[@class='wf-effra-n4-active wf-effra-n7-active wf-effra-n3-active wf-effra-n5-active wf-effra-n9-active wf-active']/body/div[@class='content overlay']/div[@class='container']/div[@class='rightPane']/div[@class='box_simple gray agentbox newhar']/div[@class='box_content grid_view']/a[1]"
-        (elemNextLnk, nFailureCnt) = self.find_wait_get_element("xpath", xpath, True)
-        time.sleep(3)
-
-        self.wait_for_new_window(self._driver)
-        window_after = self._driver.window_handles[1]
-        self._driver.close()
-        self._driver.switch_to.window(window_after)
-
         return
 
     def SignIntoMatrix(self):
         # driver = webdriver.Firefox(firefox_binary=binary)
-        print(self._cfg.getConfigValue("EntryUrl"))
+        print("Signing into {0}".format(self._cfg.getConfigValue("EntryUrl")))
         self._driver.get(self._cfg.getConfigValue("StartingUrl"))  # load the web page
         strUserName = self._cfg.getConfigValue("HARUserName")
         strPwd = self._cfg.getConfigValue("HARPassword")
@@ -40,8 +34,56 @@ class cMatrixScrapper:
         elemPwd.send_keys(strPwd)
         elemPwd.send_keys(Keys.RETURN)
 
+        (elemNextLnk, nFailureCnt) = self.find_wait_get_element("link_text", "Enter Matrix MLS")
+        window_before = self._driver.window_handles[0]
+        xpath = "/html[@class='wf-effra-n4-active wf-effra-n7-active wf-effra-n3-active wf-effra-n5-active wf-effra-n9-active wf-active']/body/div[@class='content overlay']/div[@class='container']/div[@class='rightPane']/div[@class='box_simple gray agentbox newhar']/div[@class='box_content grid_view']/a[1]"
+        (elemNextLnk, nFailureCnt) = self.find_wait_get_element("xpath", xpath, True)
+        time.sleep(3)
 
+        self.wait_for_new_window(self._driver)
+        window_after = self._driver.window_handles[1]
+        self._driver.close()
+        self._driver.switch_to.window(window_after)
+        print("signed in")
         return True
+
+
+    def wait_for_new_window(self, timeout=10):
+        handles_before = self._driver.window_handles
+        yield
+        WebDriverWait(self._driver, timeout).until(
+            lambda driver: len(handles_before) != len(driver.window_handles))
+
+    '''
+    find, wait and get element, if not successful, it will keep on trying for 10 times before quit the program
+    '''
+    def find_wait_get_element(self,elementType, val, bClick=False):
+        nFailureCount = 0
+        while nFailureCount < 5:
+            try:
+                if elementType == "link_text":
+                    elem = elemNextLnk = WebDriverWait(self._driver, 10).until(
+                        EC.presence_of_element_located((By.LINK_TEXT, val)))
+                elif elementType == "xpath":
+                    elem = elemNextLnk = WebDriverWait(self._driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH, val)))
+                elif elementType == "id":
+                    elem = elemNextLnk = WebDriverWait(self._driver, 10).until(EC.presence_of_element_located((By.ID, val)))
+                elif elementType == "partial_link_text":
+                    elem = elemNextLnk = WebDriverWait(self._driver, 10).until(
+                        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, val)))
+                elif elementType == "name":
+                    elem = elemNextLnk = WebDriverWait(self._driver, 10).until(EC.presence_of_element_located((By.NAME, val)))
+                else:
+                    elem = elemNextLnk = WebDriverWait(self._driver, 10).until(
+                        EC.presence_of_element_located((By.TAG_NAME, val)))
+                if bClick:
+                    elem.click()
+                return (elem, nFailureCount)
+            except:
+                nFailureCount += 1
+                self._driver.refresh()
+        return (None, nFailureCount)
 
     '''
         get a list of inputs and run the scrapper item by item in the list
@@ -62,10 +104,14 @@ class cMatrixScrapper:
         while bResult == False:
             bResult = self.ScrapeAllPropPage()
 
-
-    # sccrape all property page based on input
-    def ScrapeAllPropPage(self):
+'''
+     sccrape all property page based on input
+     oSearchCriteria: [(pageID, ControlType, Value)]
+'''
+    def ScrapeAllPropPage(self, oSearchCriteria):
         # load the page
+        strPageLink = "http://matrix.harmls.com/Matrix/Search/AllProperties/Classic"
+        self._driver.get(strPageLink)
 
         # Load searh criteria
 
